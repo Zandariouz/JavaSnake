5cce928ebdf4b4306a597bda7c3e53cf2bb3082f
Zandariouz <dyudayev@gmail.com>
2017-05-24 14:06:07 -0400

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -4,6 +4,15 @@ public class MainClass {
 
 	public static void main(String[] ars) {
 	
+		JFrame frame = new JFrame("Snake");
+		frame.setContentPane(new GamePanel());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setPreferredSize(new Dimension(GamePanel.WIDTH, GamePanel.HEIGHT));
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
 	}
   
 }
