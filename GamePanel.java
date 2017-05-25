33e84af239c640fd0fa9d35d7ff31bf7946f6675
Zandariouz <dyudayev@gmail.com>
2017-05-24 20:40:06 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,6 +1,15 @@
 package game;
 
-import java.awt.even.KeyEvent;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener {
 
@@ -21,6 +30,11 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	private Entity head, apple;
 	private ArrayList<Entity> snake;
 	private int score;
+	private int level;
+	private boolean gameover;
+	private int previousMove;
+	private int nextMove;
+	private int addition = 1;
 	
 	//movement
 	private int dx, dy;
@@ -29,7 +43,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	private boolean up, down, left, right, start;
 	
 	public GamePanel() {
-		setPreferedSize(new Dimension(WIDTH, HEIGHT));
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		setFocusable(true);
 		requestFocus();
 		addKeyListener(this);
@@ -94,84 +108,182 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
 		g2d = image.createGraphics();
 		running = true;
-		setLevelUp
-		setFPS(10);
+		setLevelUp();
 	}
 	
 	private void setLevelUp() {
+		
 		snake = new ArrayList<Entity>();
 		head = new Entity(SIZE);
 		head.setPosition(WIDTH / 2, HEIGHT / 2);
 		snake.add(head);
-		for(int i = 1; i < 10; i++) {
+		
+		for(int i = 1; i < 1; i++) {
 			Entity e = new Entity(SIZE);
 			e.setPosition(head.getX() + (i * SIZE), head.getY());
 			snake.add(e);
 		}
+		
+		addition = 1;
 		apple = new Entity(SIZE);
 		setApple();
 		score = 0;
+		gameover = false;
+		level = 1;
+		previousMove = -1;
+		dx = dy = 0;
+		setFPS(level * 10);
 	}
 	
 	public void setApple() {
-		int x = (int)(Math.random() * (WIDTH - SIZE));
-		int y = (int)(Math.random() * (HEIGHT - SIZE));
-		apple.setPosition(x, y);
+		if((int)(Math.random() * 3) == 0) {
+			int x = (int)(Math.random() * (WIDTH - SIZE));
+			int y = (int)(Math.random() * (HEIGHT - SIZE));
+			x = x - (x % SIZE);
+			if(x == 0)
+				x += SIZE;
+			if(x == WIDTH - SIZE)
+				x -= SIZE;
+			y = y - (y % SIZE);
+			if(y == 0)
+				y += SIZE;
+			if(y == WIDTH - SIZE)
+				y -= SIZE;
+			addition = 3;
+			apple.setSize(SIZE * 3);
+			apple.setPosition(x, y);
+		}
+		else {
+			apple.setSize(SIZE);
+			int x = (int)(Math.random() * (WIDTH - SIZE));
+			int y = (int)(Math.random() * (HEIGHT - SIZE));
+			x = x - (x % SIZE);
+			y = y - (y % SIZE);
+			addition = 1;
+			apple.setPosition(x, y);
+		}
 	}
+	
 	private void requestRender() {
-		render(g2d)
+		render(g2d);
 		Graphics g = getGraphics();
 		g.drawImage(image, 0, 0, null);
 		g.dispose();
 	}
+	
 	private void update() {
-		if(up && dy == 0) {
+		if(gameover) {
+			if(start) {
+				setLevelUp();
+			}
+			return;
+		}
+		if(up == true && dy == 0 && previousMove != 2) {
 			dy = -SIZE;
 			dx = 0;
+			nextMove = 1;
 		}
-		if(down && dy == 0) {
+		
+		if(down == true && dy == 0 && previousMove != 1) {
 			dy = SIZE;
 			dx = 0;
+			nextMove = 2;
 		}
-		if(left && dx == 0) {
+		
+		if(left == true && dx == 0 && previousMove != 4) {
 			dy = 0;
 			dx = -SIZE;
+			nextMove = 3;
 		}
-		if(right && dx == 0) {
+		
+		if(right == true && dx == 0 && previousMove != 3) {
 			dy = 0;
 			dx = SIZE;
+			nextMove = 4;
 		}
+		
 		if(dx != 0 || dy != 0) {
 			for(int i = snake.size() - 1; i > 0; i--) {
-				snake.get(i).setPosition(snake.get(i - 1).getX(), snake.get(i - 1).getX());
+				snake.get(i).setPosition(
+						snake.get(i - 1).getX(),
+						snake.get(i - 1).getY());
 			}
 			head.move(dx, dy);
 		}
-		if(apple.isCollision(head)) {
-			score++;
-			setApple();
+		
+		if(head.getX() < 0 || head.getY() < 0 || head.getX() > WIDTH || head.getX() > HEIGHT) {
+			gameover = true;
 		}
+		
+		for(Entity e : snake) {
+			if(e.isCollision(head)) {
+				gameover = true;
+				break;
+			}
+		}
+		
+		if(apple.isCollision(head)) {
+			score += addition;
+			Entity e = new Entity(SIZE);
+			e.setPosition(-100, -100);
+			snake.add(e);
+			for(int a = 1; a < addition; a++) {
+				Entity r = new Entity(SIZE);
+				r.setPosition(-100, -100);
+				snake.add(r);
+			}
+			setApple();
+				
+			if(score %10 == 0) {
+				level++;
+				if(level > 10) level = 10;
+					setFPS(level * 5 + 5);
+			}
+		}
+		
 		if(head.getX() < 0)
 			head.setX(WIDTH);
+		
 		if(head.getY() < 0)
 			head.setY(HEIGHT);
+		
 		if(head.getX() > WIDTH) 
 			head.setX(0);
+		
 		if(head.getY() > WIDTH) 
 			head.setY(0);
 	}
 	
 	public void render(Graphics2D g2d) {
+		previousMove = nextMove;
 		g2d.clearRect(0, 0, WIDTH, HEIGHT);
 		g2d.setColor(Color.RED);
+		int interval = 0;
 		for(Entity e : snake) {
+			if(interval == 5) {
+				g2d.setColor(Color.BLUE);
+				interval = 0;
+			}
 			e.render(g2d);
 			g2d.setColor(Color.WHITE);
+			interval++;
 		}
-		g2d.setColor(Color.YELLOW)
+		g2d.setColor(Color.YELLOW);
+		if(addition == 3)
+			g2d.setColor(Color.GREEN);
 		apple.render(g2d);
+		
+		if(gameover) {
+			g2d.setColor(Color.WHITE);
+			g2d.drawString("GameOver", 150, 200);
+		}
 		g2d.setColor(Color.WHITE);
-		g2d.drawString("Score : " + score, 10, 10);
+		g2d.drawString("Score : " + score + " Level : " + level, 10, 10);
+		
+		if(dx == 0 && dy == 0) {
+			g2d.setColor(Color.WHITE);
+			g2d.drawString("Ready", 150, 200);
+		}
 	}
 	
 }
